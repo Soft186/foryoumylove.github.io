97f6525dcc99be7dbf7e27e464f2cb6169c3c41d
Soft186 <lolily11826@gmail.com>
2026-05-05 23:58:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const submitPassword = document.getElementById('submitPassword');
 
     submitPassword.addEventListener('click', function() {
-        if (passwordInput.value.toLowerCase() === 'Kitty0506') {
+        if (passwordInput.value.toLowerCase() === 'babycakes') {
             passwordModal.style.opacity = '0';
             setTimeout(() => {
                 passwordModal.style.display = 'none';
